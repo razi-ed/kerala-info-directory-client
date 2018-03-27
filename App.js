a9a8d9054c5c8a455bc7c8ee0aeeaf345417298b
Razi <razi.94@outlook.com>
2018-03-27 19:44:07 +0530

    organizes the Kerala-info-directory client app

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './app/';
 
-import AppHeader from './Header'
-
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <AppHeader title={"Home"}/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f5f5f5',
-  },
-});
+export default App;
